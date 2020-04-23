cbde1c04ecd16287465699673d918b97f1d151d9
74423 <74423@DESKTOP-VT335RP>
2020-04-23 19:54:45 +0800

    添加项目文件。

---
+++ LazyMvcZhi/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LazyMvcZhi.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Show()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Del()
+        {
+            return View();
+        }
+    }
+}
